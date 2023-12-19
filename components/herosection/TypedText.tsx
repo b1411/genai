42a6c5e7f87a0dd59a380d30cf7c3a03b4d8633c
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-12-19 14:30:47 +0600

    сокращен текст

--- components/herosection/TypedText.tsx
+++ components/herosection/TypedText.tsx
@@ -15,7 +15,7 @@ export default function TypedText() {
 
     return (
         <ReactTyped
-            strings={["инновации", "максимальную эффективность", "будущее"]}
+            strings={["инновации", "эффективность", "будущее"]}
             typeSpeed={100}
             className="fw-bold text-gradient-2 typed-animation"
             loop
