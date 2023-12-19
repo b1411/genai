a1b6db80d44332be0b96a3618303a0496ec7dfa0
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-12-19 11:36:42 +0600

    init

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import RootClientLayout from "@/components/shared/layouts/RootClientLayout";
+import type { Metadata } from "next";
+import "react-toastify/dist/ReactToastify.min.css";
+import "swiper/css";
+import "swiper/css/pagination";
+// vs-code-organize-imports-disable-next-line
+import "@/scss/style.scss";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+	title: {
+		template: "%s | TANIR - лучший сервис для автоматизации продаж",
+		default: "TANIR - лучший сервис для автоматизации продаж",
+	},
+	description: "AI Based Copywriting and Content Writing Nextjs Landing Page Template",
+};
+
+type TRootLayoutProps = {
+	children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: TRootLayoutProps) {
+	return (
+		<html lang="ru" data-bs-theme="dark">
+			<head>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</head>
+			<body>
+				<RootClientLayout>
+					<div className="wrapper d-flex flex-column justify-between">{children}</div>
+				</RootClientLayout>
+				{process.env.SWITCHER_LINK ? (
+					<Link
+						href={process.env.SWITCHER_LINK}
+						className="version-switcher bg-primary position-fixed text-white"
+					>
+						Light Version
+					</Link>
+				) : null}
+			</body>
+		</html>
+	);
+}
