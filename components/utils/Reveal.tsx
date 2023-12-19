7cbf959cfb696c4cfba4ad8cafbacaf99e0187bf
Rakhmat <kenzhebaev05@gmail.com>
2023-12-19 15:53:28 +0600

    изменен заголовок отзывов

--- components/utils/Reveal.tsx
+++ components/utils/Reveal.tsx
@@ -20,7 +20,7 @@ export default function Reveal({ el, children, delay, className, repeat, ...rest
 			initial={{ opacity: 0, translateY: 20, transitionProperty: "transform opacity" }}
 			whileInView={{ opacity: 1, translateY: 0 }}
 			viewport={{ once: true }}
-			transition={{ duration: 0.5, delay: delay, repeat: repeat ? repeat : 1 }}
+			transition={{ duration: 0.5, delay: delay }}
 			className={className}
 			{...rest}
 		>
