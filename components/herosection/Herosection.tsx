012d441991159c18c3e42ac15b9f954790c50674
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-12-19 14:47:25 +0600

    сокращен текст

--- components/herosection/Herosection.tsx
+++ components/herosection/Herosection.tsx
@@ -10,59 +10,60 @@ import Reveal from "../utils/Reveal";
 import TypedText from "./TypedText";
 
 export default function Herosection() {
-	return (
-		<section
-			className="hero-section style-1 overflow-hidden bg-dark pt-30 pb-15 pb-lg-20 pt-xl-36"
-			data-bs-theme="dark"
-		>
-			<div className="container">
-				<div className="row justify-center">
-					<div className="col-lg-9">
-						<div className="text-center">
-							<div className="position-relative z-1">
-								<Reveal el="p" className="text-primary-dark">
-									AI Интегратор
-								</Reveal>
-								<Reveal el="h1" className="text-white mb-8" delay={0.15}>
-									TANIR TECH это про <br />
-									<TypedText />
-								</Reveal>
-								<Reveal delay={0.3}>
-									<Link href="/contact" className="btn btn-lg btn-gradient-1">
-										Обратиться к нам
-									</Link>
-								</Reveal>
-							</div>
-							<Reveal delay={0.45}>
-								<div className="image-with-shape">
-									<Image
-										src={shapeImage}
-										alt="shape"
-										className="shape animate-scale"
-									/>
-									<div className="mt-12 rounded-5 border border-primary-dark shadow-lg overflow-hidden position-relative z-1">
-										<Image
-											placeholder="blur"
-											src={heroImage}
-											alt="hero image"
-											className="img-fluid d-inline-block"
-										/>
-									</div>
-								</div>
-							</Reveal>
-							<ul className="d-flex flex-wrap gap-4 gap-md-8 gap-lg-10 align-center justify-center mt-8 mb-0">
-								<li>Высокая эффективность</li>
-								<li>Непрерывная работа</li>
-								<li>Минимум ошибок</li>
-							</ul>
-							<div className="d-flex gap-8 align-center justify-center mt-12 review-badges">
-								<Image src={reviewIcon1} alt="icon" className="img-fluid" />
-								<Image src={reviewIcon2} alt="icon" className="img-fluid" />
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+    return (
+        <section
+            className="hero-section style-1 overflow-hidden bg-dark pt-30 pb-15 pb-lg-20 pt-xl-36"
+            data-bs-theme="dark"
+        >
+            <div className="container">
+                <div className="row justify-center">
+                    <div className="col-lg-9">
+                        <div className="text-center">
+                            <div className="position-relative z-1">
+                                <Reveal el="p" className="text-primary-dark">
+                                    AI Интегратор
+                                </Reveal>
+                                <Reveal el="h1" className="text-white mb-8" delay={0.15}>
+                                    TANIR TECH <br/>
+                                    <TypedText/>
+                                </Reveal>
+                                <Reveal delay={0.3}>
+                                    <Link href="/contact" className="btn btn-lg btn-gradient-1">
+                                        Обратиться к нам
+                                    </Link>
+                                </Reveal>
+                            </div>
+                            <Reveal delay={0.45}>
+                                <div className="image-with-shape">
+                                    <Image
+                                        src={shapeImage}
+                                        alt="shape"
+                                        className="shape animate-scale"
+                                    />
+                                    <div
+                                        className="mt-12 rounded-5 border border-primary-dark shadow-lg overflow-hidden position-relative z-1">
+                                        <Image
+                                            placeholder="blur"
+                                            src={heroImage}
+                                            alt="hero image"
+                                            className="img-fluid d-inline-block"
+                                        />
+                                    </div>
+                                </div>
+                            </Reveal>
+                            <ul className="d-flex flex-wrap gap-4 gap-md-8 gap-lg-10 align-center justify-center mt-8 mb-0">
+                                <li>Высокая эффективность</li>
+                                <li>Непрерывная работа</li>
+                                <li>Минимум ошибок</li>
+                            </ul>
+                            <div className="d-flex gap-8 align-center justify-center mt-12 review-badges">
+                                <Image src={reviewIcon1} alt="icon" className="img-fluid"/>
+                                <Image src={reviewIcon2} alt="icon" className="img-fluid"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
