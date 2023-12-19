82219e926ef1e69049485812bef87e168702c098
Kurlik <johndoe@example.com>
2023-12-19 14:54:18 +0600

    изменение вопроса

--- data/faq.ts
+++ data/faq.ts
@@ -13,8 +13,8 @@ export const dataFaqs: TFaq[] = [
     },
     {
         id: 3,
-        header: "Какие данные требуются для обучения ИИ-ассистента под конкретные потребности и характер работы бизнеса??",
-        body: "Once you know your audience, choose a topic that will resonate with them.Look for trending topics in your industry or address common questions or challenges your audience may be facing.Keep in mind that your topic should be both interesting and relevant to your audience",
+        header: "Какие данные требуются для обучения ИИ-ассистента под конкретные потребности и характер работы бизнеса?",
+        body: "ИИ может требовать доступа к различным данным, включая исторические данные о работе компании, информацию о клиентах, транзакционные данные и т.д.",
     },
     {
         id: 4,
