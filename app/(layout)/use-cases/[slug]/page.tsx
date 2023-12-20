3bf97525f044773cc31d5ebc1984341c5249214b
Rakhmat <kenzhebaev05@gmail.com>
2023-12-20 12:06:00 +0600

    formatted

--- app/(layout)/use-cases/[slug]/page.tsx
+++ app/(layout)/use-cases/[slug]/page.tsx
@@ -11,104 +11,102 @@ import { Metadata } from "next";
 import Image from "next/image";
 
 type TBlogDetailsPageProps = {
-	params: {
-		slug: string;
-	};
+    params: {
+        slug: string;
+    };
 };
 
 type TGenerateMetadataProps = {
-	params: {
-		slug: string;
-	};
+    params: {
+        slug: string;
+    };
 };
 
 export function generateMetadata({ params }: TGenerateMetadataProps): Metadata {
-	const { slug: slugFromUrl } = params;
-	const { title } = getusecaseData(slugFromUrl);
+    const { slug: slugFromUrl } = params;
+    const { title } = getusecaseData(slugFromUrl);
 
-	return {
-		title: title,
-	};
+    return {
+        title: title,
+    };
 }
 
 export default function UsecaseDetails({ params }: TBlogDetailsPageProps) {
-	const { slug: slugFromUrl } = params;
-	const { title, description, slug, content } = getusecaseData(slugFromUrl);
+    const { slug: slugFromUrl } = params;
+    const { title, description, slug, content } = getusecaseData(slugFromUrl);
 
-	return (
-		<main className="flex-grow-1">
-			<Breadcrumb
-				title={title}
-				path={[
-					{
-						text: "Home",
-						link: "/",
-					},
-					{
-						text: "Use cases",
-						link: "/use-cases",
-					},
-					{
-						text: title,
-					},
-				]}
-			/>
+    return (
+        <main className="flex-grow-1">
+            <Breadcrumb
+                title={title}
+                path={[
+                    {
+                        text: "Home",
+                        link: "/",
+                    },
+                    {
+                        text: "Примеры применения",
+                        link: "/use-cases",
+                    },
+                    {
+                        text: title,
+                    },
+                ]}
+            />
 
-			<section className="pt-10 pt-lg-15">
-				<div className="container">
-					<div className="row align-center">
-						<Reveal className="col-lg-6 col-xl-5" delay={0.05}>
-							<div className="text-center text-lg-start">
-								<h1 className="mb-4 text-white">
-									Complete a blog entry in half the time.
-								</h1>
-								<p className="mb-8">
-									With a few clicks of a button, you can create a whole outline,
-									opening paragraph, and body for your blog.
-								</p>
-								<div className="w-xl-3quarter">
-									<Newsletter />
-								</div>
-							</div>
-						</Reveal>
-						<Reveal className="col-lg-6 offset-xl-1" delay={0.1}>
-							<div className="text-center">
-								<Image
-									placeholder="blur"
-									src={chatScreenImage}
-									alt="image"
-									className="img-fluid d-inline-block"
-								/>
-							</div>
-						</Reveal>
-					</div>
-					<hr className="border-top border-dark-blue opacity-100 my-18" />
-					<div className="row justify-center">
-						<div className="col-lg-8">
-							<Markdown
-								options={{
-									wrapper: "article",
-									overrides: {
-										img: {
-											props: {
-												className: "img-fluid rounded",
-											},
-										},
-									},
-								}}
-							>
-								{content}
-							</Markdown>
-						</div>
-					</div>
-				</div>
-			</section>
+            <section className="pt-10 pt-lg-15">
+                <div className="container">
+                    <div className="row align-center">
+                        <Reveal className="col-lg-6 col-xl-5" delay={0.05}>
+                            <div className="text-center text-lg-start">
+                                <h1 className="mb-4 text-white">Будьте В Курсе</h1>
+                                <p className="mb-8">
+                                    Подпишитесь на наши новостные обновления и оставайтесь впереди в
+                                    развитии AI-технологий и инноваций
+                                </p>
+                                <div className="w-xl-3quarter">
+                                    <Newsletter />
+                                </div>
+                            </div>
+                        </Reveal>
+                        <Reveal className="col-lg-6 offset-xl-1" delay={0.1}>
+                            <div className="text-center">
+                                <Image
+                                    placeholder="blur"
+                                    src={chatScreenImage}
+                                    alt="image"
+                                    className="img-fluid d-inline-block"
+                                />
+                            </div>
+                        </Reveal>
+                    </div>
+                    <hr className="border-top border-dark-blue opacity-100 my-18" />
+                    <div className="row justify-center">
+                        <div className="col-lg-8">
+                            <Markdown
+                                options={{
+                                    wrapper: "article",
+                                    overrides: {
+                                        img: {
+                                            props: {
+                                                className: "img-fluid rounded",
+                                            },
+                                        },
+                                    },
+                                }}
+                            >
+                                {content}
+                            </Markdown>
+                        </div>
+                    </div>
+                </div>
+            </section>
 
-			<ReviewsSection />
+            <ReviewsSection />
 
-			<FaqSection />
+            <FaqSection />
 
-			<CtaHome />
-		</main>
-	);
+            <CtaHome />
+        </main>
+    );
 }
