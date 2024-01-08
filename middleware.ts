7cc7bdd127dec55523911efbe7627328cad18432
Rakhmat <kenzhebaev05@gmail.com>
2024-01-08 15:16:26 +0600

    fixed middleware

--- middleware.ts
+++ middleware.ts
@@ -1,37 +1,56 @@
 import { match } from "@formatjs/intl-localematcher";
 import Negotiator from "negotiator";
-import type { NextRequest, NextResponse } from "next/server";
+import next from "next";
+import { NextRequest, NextResponse } from "next/server";
+import path, { parse } from "path";
 
 let locales = ["en", "ru"];
 
-function getLocale(request: NextRequest): string {
-    let headers = {
-        "accept-language": "ru,en",
-    };
-
-    let languages = new Negotiator({ headers }).languages();
+function getLocale(request: NextRequest, pathname: string): string {
     let locales = ["en", "ru"];
-    let defaultLocale = "en-US";
+    let defaultLocale = "en";
+
+    const negotiatorHeaders: Record<string, string> = {};
+    request.headers.forEach((value, key) => {
+        negotiatorHeaders[key] = value;
+    });
+
+    let languages = new Negotiator({ headers: negotiatorHeaders }).languages(locales);
 
     let acceptedLanguage = match(languages, locales, defaultLocale);
     return acceptedLanguage;
 }
 
+function parseLocaleFromReferer(request: NextRequest): string | null {
+    const referer = request.headers.get("referer");
+    if (!referer) {
+        return null;
+    }
+
+    const url = new URL(referer);
+    const locale = url.pathname.split("/")[1];
+    return locales.includes(locale) ? locale : null;
+}
+
 export function middleware(request: NextRequest) {
-    // Check if there is any supported locale in the pathname
-    const { pathname } = request.nextUrl;
-    const pathnameHasLocale = locales.some(
-        (locale: string) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`,
-    );
+    let pathname = request.nextUrl.pathname;
+    let targetLocale = null;
 
-    if (pathnameHasLocale) return;
+    // Check if the URL contains a locale
+    if (locales.includes(pathname.split("/")[1])) {
+        targetLocale = pathname.split("/")[1];
+        console.log("URL locale: " + targetLocale);
+        return
+    } else if (parseLocaleFromReferer(request)) {
+        targetLocale = parseLocaleFromReferer(request);
+        console.log("Referer locale: " + targetLocale);
+    } else {
+        targetLocale = getLocale(request, pathname);
+        console.log("Locale: " + targetLocale);
+    }
 
-    // Redirect if there is no locale
-    const locale = getLocale(request);
-    request.nextUrl.pathname = `/${locale}${pathname}`;
-    // e.g. incoming request is /products
-    // The new URL is now /en-US/products
-    return Response.redirect(request.nextUrl);
+    request.nextUrl.pathname = `/${targetLocale}${pathname}`;
+    return NextResponse.redirect(request.nextUrl);
 }
 
 export const config = {
