8e796a79a6d98077890a4899008b0cb1a765cc43
Rakhmat <kenzhebaev05@gmail.com>
2024-01-08 19:27:34 +0600

    root pages replaced

--- app/[lang]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import {JSX} from "react";
-
-export default function Loading(): JSX.Element {
-    return (
-        <div className="loading">
-            <div className="lds-dual-ring"></div>
-        </div>
-    );
-}
